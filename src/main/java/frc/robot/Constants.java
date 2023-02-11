465241ad9e4c96fe1a8a451392d6841ff2f4d1c6
stonem2 <stonem2@muhs.edu>
2023-02-11 14:30:24 -0600

    Cleaned up the competition code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,9 +8,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.RobotBase;
-import frc.lib.team6328.util.Alert;
-import frc.lib.team6328.util.Alert.AlertType;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -37,51 +34,26 @@ public final class Constants {
   public static final int INDEXER_ROTATION_CAN_ID = 51; // FIXME: get correct can id
   public static final int INDEXER_GRABBER_CAN_ID = 52; // FIXME:  get correct can id
 
-  // FIXME: specify the name of the camera used for detecting AprilTags
   public static final String CAMERA_NAME = "ov9268";
 
-  private static final RobotType ROBOT = RobotType.ROBOT_2023_PRESEASON;
+  private static final RobotType ROBOT = RobotType.ROBOT_2023_PRESEASON; //TODO: toggle as nessecary
+  //private static final RobotType ROBOT = RobotType.ROBOT_2023_COMPETITION; 
 
-  private static final Alert invalidRobotAlert =
-      new Alert("Invalid robot selected, using competition robot as default.", AlertType.ERROR);
 
-  // FIXME: update for various robots
   public static RobotType getRobot() {
-    if (RobotBase.isReal()) {
-      if (ROBOT == RobotType.ROBOT_SIMBOT) { // Invalid robot selected
-        invalidRobotAlert.set(true);
-        return RobotType.ROBOT_2023_PRESEASON;
-      } else {
-        return ROBOT;
-      }
-    } else {
-      return ROBOT;
-    }
+    return ROBOT;
   }
 
-  // FIXME: update for various robots
   public static Mode getMode() {
-    switch (getRobot()) {
-      case ROBOT_2023_PRESEASON:
-        return RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
-
-      case ROBOT_SIMBOT:
-        return Mode.SIM;
-
-      default:
-        return Mode.REAL;
-    }
+    return Mode.REAL;
   }
 
-  // FIXME: update for various robots
   public enum RobotType {
     ROBOT_2023_PRESEASON,
-    ROBOT_SIMBOT
+    ROBOT_2023_COMPETITION;
   }
 
   public enum Mode {
-    REAL,
-    REPLAY,
-    SIM
+    REAL
   }
 }
