e63c33fb60fdbba288ef021b41a783ba2679c628
stonem2 <stonem2@muhs.edu>
2023-02-21 20:37:14 -0600

    delete handler folder

--- src/main/java/frc/robot/commands/SmartIntakeCommand.java
+++ src/main/java/frc/robot/commands/SmartIntakeCommand.java
@@ -1,12 +1,9 @@
 package frc.robot.commands;
 
-
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.state_machine.RobotStateMachine;
 import frc.robot.state_machine.events.IntakePressed;
-import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class SmartIntakeCommand extends CommandBase {
@@ -17,21 +14,17 @@ public class SmartIntakeCommand extends CommandBase {
     addRequirements(intakeSubsystem);
   }
 
-  public void initialize() {
-
-  }
+  public void initialize() {}
 
   @Override
-  public void execute() {
-  }
-  
+  public void execute() {}
 
   public void end(boolean interrupted) {
-    RobotStateMachine.getInstance().fireEvent(new IntakePressed()); //TODO: make piece detected
+    RobotStateMachine.getInstance().fireEvent(new IntakePressed()); // TODO: make piece detected
   }
 
   public boolean isFinished() {
-    //return intakeSubsytem.hasPiece();
+    // return intakeSubsytem.hasPiece();
     return true;
   }
 }
