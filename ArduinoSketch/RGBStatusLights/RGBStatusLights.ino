c1a44027527fe818eba5781d2a3d8c672b0113ca
Alan Bork <aebork@gmail.com>
2023-02-27 22:12:24 -0600

    Refactoring and other misc. improvements to the RGB Status

---
+++ ArduinoSketch/RGBStatusLights/RGBStatusLights.ino
@@ -0,0 +1,224 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_DATA 2
+
+#define NUMPIXELS 69  // number of neopixels in strip
+#define DELAY_TIME 200
+#define INTENSITY 150
+
+Adafruit_NeoPixel pixels(NUMPIXELS, LED_DATA, NEO_GRB + NEO_KHZ800);
+
+#define DIGITAL_D0 8
+#define DIGITAL_D1 9
+#define DIGITAL_D2 10
+#define DIGITAL_D3 11
+#define DIGITAL_D4 12
+
+#define OUTPUT_D0 3
+#define OUTPUT_D1 4
+#define OUTPUT_D2 5
+#define OUTPUT_D3 6
+#define OUTPUT_D4 7
+
+int mode = 0;
+int timer = 0;
+
+void setup() {
+  Serial.begin(250000);
+
+  pinMode(DIGITAL_D0, INPUT_PULLUP);
+  pinMode(DIGITAL_D1, INPUT_PULLUP);
+  pinMode(DIGITAL_D2, INPUT_PULLUP);
+  pinMode(DIGITAL_D3, INPUT_PULLUP);
+  pinMode(DIGITAL_D4, INPUT_PULLUP);
+
+  pinMode(OUTPUT_D0, OUTPUT);
+  pinMode(OUTPUT_D1, OUTPUT);
+  pinMode(OUTPUT_D2, OUTPUT);
+  pinMode(OUTPUT_D3, OUTPUT);
+  pinMode(OUTPUT_D4, OUTPUT);
+
+  pixels.begin();
+}
+
+void loop() {
+  bool b0, b1, b2, b3, b4;
+
+  // HIGH is 0, LOW is 1 on the inputs
+  b0 = !digitalRead(DIGITAL_D0);
+  b1 = !digitalRead(DIGITAL_D1);
+  b2 = !digitalRead(DIGITAL_D2);
+  b3 = !digitalRead(DIGITAL_D3);
+  b4 = !digitalRead(DIGITAL_D4);
+
+  digitalWrite(OUTPUT_D0, b0);
+  digitalWrite(OUTPUT_D1, b1);
+  digitalWrite(OUTPUT_D2, b2);
+  digitalWrite(OUTPUT_D3, b3);
+  digitalWrite(OUTPUT_D4, b4);
+
+  mode = ((int)b0 << 0) + ((int)b1 << 1) + ((int)b2 << 2) + ((int)b3 << 3) + ((int)b4 << 4);
+  Serial.print("Mode: ");
+  Serial.println(mode);
+
+  switch (mode) {
+    case 0:
+      // no signal sent
+      setColor(false, false, false);
+      break;
+
+    case 1:
+      // Score Low - no game piece
+      setColorLow(false, true, false);
+      break;
+
+    case 2:
+      // Score Mid - no game piece
+      setColorMid(false, true, false);
+      break;
+
+    case 3:
+      // Score High - no game piece
+      setColorHigh(false, true, false);
+      break;
+
+    case 4:
+      // No Score Cube
+      setColor(true, false, true);
+      break;
+
+    case 5:
+      // Cube Low
+      setColorLow(true, false, true);
+      break;
+
+    case 6:
+      // Mid Cube
+      setColorMid(true, false, true);
+      break;
+
+    case 7:
+      // High Cube
+      setColorHigh(true, false, true);
+      break;
+
+    case 8:
+      // No Score Cone
+      setColor(true, true, false);
+      break;
+
+    case 9:
+      // Cone Low
+      setColorLow(true, true, false);
+      break;
+
+    case 10:
+      // Cone Mid
+      setColorMid(true, true, false);
+      break;
+
+    case 11:
+      // Cone High
+      setColorHigh(true, true, false);
+      break;
+
+    case 12:
+      // Blue and Yellow
+      setColorBY(false, false, true);
+      break;
+
+    case 17:
+      // Game piece Captured
+      flashFast(false, true, false);
+      break;
+
+    default:
+      // change nottin'
+      break;
+  }
+
+  timer++;
+  delay(1);
+}
+
+void setColor(bool red, bool green, bool blue) {
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)red, INTENSITY * (int)green, INTENSITY * (int)blue));
+  }
+  pixels.show();
+}
+
+void blinkColor(bool red, bool green, bool blue) {
+  if (timer < 150) {
+    setColor(red, green, blue);
+  }
+
+  if (timer < 300 && timer > 150) {
+    setColor(false, false, false);
+  }
+
+  if (timer > 301) {
+    timer = 0;
+  }
+}
+
+void flashFast(bool red, bool green, bool blue) {
+  if (timer < 60) {
+    setColor(red, green, blue);
+  }
+
+  if (timer < 120 && timer > 60) {
+    setColor(false, false, false);
+  }
+
+  if (timer > 121) {
+    timer = 0;
+  }
+}
+
+void setColorLow(bool red, bool green, bool blue) {
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if (i < 23) {
+      pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)red, INTENSITY * (int)green, INTENSITY * (int)blue));
+    }
+  }
+  pixels.show();
+}
+
+void setColorMid(bool red, bool green, bool blue) {
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if (i < 46 && i > 22) {
+      pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)red, INTENSITY * (int)green, INTENSITY * (int)blue));
+    }
+  }
+  pixels.show();
+}
+
+void setColorHigh(bool red, bool green, bool blue) {
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if (i < NUMPIXELS && i > 45) {
+      pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)red, INTENSITY * (int)green, INTENSITY * (int)blue));
+    }
+  }
+  pixels.show();
+}
+
+void setColorBY(bool red, bool green, bool blue) {
+  bool invRed = !red;
+  bool invGreen = !green;
+  bool invBlue = !blue;
+  pixels.clear();
+
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if ((i / 4) % 2 == 0) {
+      pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)red, INTENSITY * (int)green, INTENSITY * (int)blue));
+    } else {
+      pixels.setPixelColor(i, pixels.Color(INTENSITY * (int)invRed, INTENSITY * (int)invGreen, INTENSITY * (int)invBlue));
+    }
+  }
+  pixels.show();
+}
--- src/main/java/frc/robot/subsystems/RGBStatusSubsytem.java
+++ src/main/java/frc/robot/subsystems/RGBStatusSubsytem.java
@@ -5,75 +5,125 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RGBStatusSubsytem extends SubsystemBase {
+  /*-
+   * Outputs 0 and 1 set the scoring height.
+   *   Low Score - Bit 0
+   *   Mid Score - Bit 1
+   *   High Score - Bits 0 and 1
+   *
+   * Outputs 2 amd 3 set the game piece
+   *   Cone - Bit 3
+   *   Cube - Bit 2
+   *
+   * Output 4 is the modifer for every other mode we want to represent
+   *   Capture Game Piece - Bit 4 and 0
+   *
+   */
 
-  private DigitalOutput out0 = new DigitalOutput(0);
-  private DigitalOutput out1 = new DigitalOutput(1);
-  private DigitalOutput out2 = new DigitalOutput(2);
-  private DigitalOutput out3 = new DigitalOutput(3);
-  private DigitalOutput out4 = new DigitalOutput(4);
+  private DigitalOutput out0 = new DigitalOutput(1);
+  private DigitalOutput out1 = new DigitalOutput(2);
+  private DigitalOutput out2 = new DigitalOutput(3);
+  private DigitalOutput out3 = new DigitalOutput(4);
+  private DigitalOutput out4 = new DigitalOutput(5);
+
   private ScoreColors scoreColors;
   private GamePiece gamePiece;
+
+  private Timer timer;
+  private SpecialMode specialMode;
+  private double targetElapsedTimeSeconds;
 
   /** Creates a new RGBStatus. */
   public RGBStatusSubsytem() {
     scoreColors = ScoreColors.NONE;
     gamePiece = GamePiece.NONE;
+    specialMode = SpecialMode.NONE;
+    targetElapsedTimeSeconds = 0;
+    timer = new Timer();
   }
 
   @Override
   public void periodic() {
-    switch (scoreColors) {
-      case HIGH:
-        out0.set(true);
-        out1.set(true);
-        break;
+    // Invert the digital sigs for HIGH is 0, LOW is 1
+    if (specialMode != SpecialMode.NONE) {
+      if (timer.hasElapsed(targetElapsedTimeSeconds)) {
+        specialMode = SpecialMode.NONE;
+        timer.stop();
+      } else {
+        switch (specialMode) {
+          case GAME_PIECE_CAPTURED:
+            // bits 4 and 0 -- 17
+            out4.set(!true);
+            out0.set(!true);
+            break;
 
-      case LOW:
-        out0.set(true);
-        out1.set(false);
-        break;
-
-      case MEDIUM:
-        out0.set(false);
-        out1.set(true);
-        break;
-
-      case NONE:
-      default:
-        out0.set(false);
-        out1.set(false);
-        break;
+          case NONE:
+          default:
+            break;
+        }
+      }
     }
 
-    switch (gamePiece) {
-      case CONE:
-        out2.set(false);
-        out3.set(true);
-        break;
+    if (specialMode == SpecialMode.NONE) {
+      switch (scoreColors) {
+        case HIGH: // 3
+          out0.set(!true);
+          out1.set(!true);
+          break;
 
-      case CUBE:
-        out2.set(true);
-        out3.set(false);
-        break;
+        case LOW: // 1
+          out0.set(!true);
+          out1.set(!false);
+          break;
 
-      case NONE:
-      default:
-        out2.set(false);
-        out3.set(false);
-        break;
+        case MEDIUM: // 2
+          out0.set(!false);
+          out1.set(!true);
+          break;
+
+        case NONE:
+        default:
+          out0.set(!false);
+          out1.set(!false);
+          break;
+      }
+
+      switch (gamePiece) {
+        case CONE: // +8
+          out2.set(!false);
+          out3.set(!true);
+          break;
+
+        case CUBE: // +4
+          out2.set(!true);
+          out3.set(!false);
+          break;
+
+        case NONE:
+        default:
+          out2.set(!false);
+          out3.set(!false);
+          break;
+      }
+      out4.set(!false);
     }
-
-    out4.set(!false);
   }
 
-  // public void hasPiece(boolean gamePiece) {}
+  public void captureGamePiece() {
+    timer.reset();
+    timer.start();
+    targetElapsedTimeSeconds = 2.0;
+    specialMode = SpecialMode.GAME_PIECE_CAPTURED;
+  }
 
   public void off() {
     scoreColors = ScoreColors.NONE;
     gamePiece = GamePiece.NONE;
+    specialMode = SpecialMode.NONE;
   }
 
   public void humanSignals(GamePiece gamePiece) {
@@ -95,5 +145,10 @@ public class RGBStatusSubsytem extends SubsystemBase {
     CONE,
     CUBE,
     NONE;
+  }
+
+  protected enum SpecialMode {
+    NONE,
+    GAME_PIECE_CAPTURED;
   }
 }
