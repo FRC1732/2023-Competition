f180258ccaada0760b95b3648b1d50955deac157
Alan Bork <aebork@gmail.com>
2023-03-01 21:41:42 -0600

    Basic implementation of limelight scoring

--- src/main/java/frc/robot/subsystems/LimelightScoring.java
+++ src/main/java/frc/robot/subsystems/LimelightScoring.java
@@ -6,12 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.HttpCamera;
-import edu.wpi.first.cscore.VideoSink;
+import edu.wpi.first.cscore.MjpegServer;
 import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -20,41 +17,32 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
+import java.util.Map;
 
 public class LimelightScoring extends SubsystemBase {
   // limelight hostname: http://limelight-scoring.local:5801
   private final String LIMELIGHTNAME = "limelight-scoring";
-  private NetworkTable table;
-  private NetworkTableEntry llData_camerastream;
-  private NetworkTableEntry tv;
-  private NetworkTableEntry tx;
-  private NetworkTableEntry ty;
-  private NetworkTableEntry ta;
-  private NetworkTableEntry ledMode;
-  private NetworkTableEntry camMode;
-
-  private double r_tv;
-  private double r_tx;
-  private double r_ty;
-  private double r_ta;
+  private final Pose2d EMPTY_POSE2D = new Pose2d();
 
   private HttpCamera LLFeed;
-  private VideoSink server2;
-  private int cameraStream = 0;
+  private MjpegServer server;
 
-  private ProfiledPIDController _thetaController;
-  private GenericEntry rotationP;
-  private GenericEntry rotationI;
-  private GenericEntry rotationD;
-
-  private final Pose2d EMPTY_POSE2D = new Pose2d();
   private LimelightHelpers.LimelightResults llresults;
   private boolean havePosition = false;
   private Pose2d position = EMPTY_POSE2D;
 
+  private NetworkTable table;
+  private NetworkTableEntry tv;
+  private NetworkTableEntry tx;
+  private NetworkTableEntry ty;
+  private NetworkTableEntry pipeline;
+
   private ScoringMode currentScoringMode = ScoringMode.Undefined;
+
+  private double reflectiveTv;
+  private double reflectiveTx;
+  private double reflectiveTy;
+  private int pipelineVal;
 
   /** Creates a new Limelight. */
   public LimelightScoring() {
@@ -62,152 +50,81 @@ public class LimelightScoring extends SubsystemBase {
     configureShuffleBoard();
   }
 
-  public void on() {
-    // ledMode.setNumber(LL_LEDSTATE_ON);
-  }
-
-  public void off() {
-    // ledMode.setNumber(LL_LEDSTATE_OFF);
-  }
-
   private void configureNetworkTableEntries() {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
+    table = NetworkTableInstance.getDefault().getTable(LIMELIGHTNAME);
     tv = table.getEntry("tv");
     tx = table.getEntry("tx");
     ty = table.getEntry("ty");
-    ta = table.getEntry("ta");
-    ledMode = table.getEntry("ledMode");
-    camMode = table.getEntry("camMode");
+    pipeline = table.getEntry("pipeline");
   }
 
   private void configureShuffleBoard() {
-    ShuffleboardTab tab;
-    LLFeed = new HttpCamera("limelight", "http://10.17.32.11:5800/stream.mjpg");
-    server2 = CameraServer.getServer("serve_USB Camera 0");
-    LLFeed.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+    if (Constants.DEBUGGING) {
+      ShuffleboardTab tab;
+      tab = Shuffleboard.getTab(LIMELIGHTNAME);
 
-    tab = Shuffleboard.getTab("limelight");
-    tab.addNumber("LED Mode", ll_ledModeSupplier);
-    tab.addNumber("tv - Valid Targets", ll_tvSupplier);
-    tab.addNumber("tx - Horiz Offset", ll_txSupplier);
-    tab.addNumber("ty - Vert Offset", ll_tySupplier);
-    tab.addBoolean("Target Acquired", ll_hasTarget);
-    rotationP =
-        tab.add("rotation p", 0).withWidget(BuiltInWidgets.kTextView).withSize(1, 1).getEntry();
-    rotationI =
-        tab.add("rotation I", 0).withWidget(BuiltInWidgets.kTextView).withSize(1, 1).getEntry();
-    rotationD =
-        tab.add("rotation D", 0).withWidget(BuiltInWidgets.kTextView).withSize(1, 1).getEntry();
+      LLFeed = new HttpCamera(LIMELIGHTNAME, "http://10.17.32.11:5800/stream.mjpg");
+      server = CameraServer.addSwitchedCamera("Object Camera");
+      server.setSource(LLFeed);
+      LLFeed.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+      tab.add(server.getSource())
+          .withWidget(BuiltInWidgets.kCameraStream)
+          .withPosition(5, 0)
+          .withSize(5, 5)
+          .withProperties(Map.of("Show Crosshair", true, "Show Controls", false));
+
+      tab.addNumber("tv - Valid Targets", () -> reflectiveTv);
+      tab.addNumber("tx - Horiz Offset", () -> reflectiveTx);
+      tab.addNumber("ty - Vert Offset", () -> reflectiveTy);
+      tab.addBoolean("Target Acquired", () -> reflectiveTv > 0);
+
+      tab.addNumber("Tag Sighted", () -> getPriorityTag());
+      tab.addString("Robot Pose", () -> getPose2d().toString());
+
+      tab.addNumber("Selected Pipeline", () -> pipelineVal);
+    } else {
+      // competition shuffleboard
+      // what do we want here?
+    }
   }
-
-  DoubleSupplier ll_ledModeSupplier =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          return 0;
-        }
-      };
-
-  DoubleSupplier ll_tvSupplier =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          return r_tv;
-        }
-      };
-
-  DoubleSupplier ll_txSupplier =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          return r_tx;
-        }
-      };
-
-  DoubleSupplier ll_tySupplier =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          return r_ty;
-        }
-      };
-
-  DoubleSupplier ll_taSupplier =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          return r_ta;
-        }
-      };
-
-  public void nullifyPID() {
-    _thetaController = null;
-  }
-
-  public DoubleSupplier rotation =
-      new DoubleSupplier() {
-        @Override
-        public double getAsDouble() {
-          if (!hasTarget()) return 0;
-          double targetRad = Math.toRadians(getTx());
-          if (_thetaController == null) {
-            var profileConstraints =
-                new TrapezoidProfile.Constraints(
-                    Constants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
-                    Constants.MAX_ANGULAR_ACCELERATION * Math.PI / 180 * 5);
-            _thetaController = new ProfiledPIDController(13, 0, 1, profileConstraints);
-            _thetaController.enableContinuousInput(Math.PI * -1, Math.PI);
-            _thetaController.reset(targetRad);
-          }
-
-          return _thetaController.calculate(targetRad, 0);
-        }
-      };
-
-  BooleanSupplier ll_hasTarget =
-      new BooleanSupplier() {
-        @Override
-        public boolean getAsBoolean() {
-          return hasTarget();
-        }
-      };
 
   @Override
   public void periodic() {
     // read and store values periodically
-    llresults = LimelightHelpers.getLatestResults("");
+    if (currentScoringMode == ScoringMode.AprilTag) {
+      llresults = LimelightHelpers.getLatestResults(LIMELIGHTNAME);
 
-    try {
-      position = llresults.targetingResults.getBotPose2d();
-    } catch (NullPointerException e) {
-      position = EMPTY_POSE2D;
-    }
+      try {
+        position = llresults.targetingResults.getBotPose2d();
+      } catch (NullPointerException e) {
+        position = EMPTY_POSE2D;
+      }
 
-    havePosition = EMPTY_POSE2D.equals(position);
+      havePosition = EMPTY_POSE2D.equals(position);
 
-    if (havePosition) {
-      r_tx = position.getX();
-      r_ty = position.getY();
-      r_ta = 1;
-      r_tv = havePosition ? 1 : 0;
+      if (llresults != null) {
+        // System.out.println(LimelightHelpers.getJSONDump(""));
+      }
     } else {
-      r_tx = -1;
-      r_ty = -1;
-      r_ta = -1;
-      r_tv = -1;
+      // ReflectiveTape or Undefined
+      reflectiveTv = tv.getDouble(0);
+      reflectiveTx = tx.getDouble(0);
+      reflectiveTy = ty.getDouble(0);
     }
-
-    if (llresults != null) {
-      // System.out.println(LimelightHelpers.getJSONDump(""));
-    }
+    pipelineVal = (int) pipeline.getDouble(-1);
   }
 
+  /**
+   * Provides result for if a reflective tape target has been sighted
+   *
+   * @return true if reflective tape target found
+   */
   public boolean hasTarget() {
-    return r_tv > 0;
+    return reflectiveTv > 0;
   }
 
   public int getPriorityTag() {
-    if (hasTarget()) {
+    if (havePosition) {
       return (int) llresults.targetingResults.targets_Fiducials[0].fiducialID;
     }
 
@@ -218,16 +135,54 @@ public class LimelightScoring extends SubsystemBase {
     return Math.abs(getTx()) < 2.5;
   }
 
+  /**
+   * Provides X (horizontal) value from limelight target when in Reflective Tape mode. Undefined
+   * when in AprilTag mode.
+   *
+   * @return a horizontal "distance" from center of camera. Positive to the right.
+   */
   public Double getTx() {
-    return r_tx;
+    return reflectiveTx;
   }
 
+  /**
+   * Provides Y (vertical) value from limelight target when in Reflective Tape mode. Undefined when
+   * in AprilTag mode.
+   *
+   * @return a vertical "distance" from center of camera. Positive is up.
+   */
   public Double getTy() {
-    return r_ty;
+    return reflectiveTy;
   }
 
+  /**
+   * Provide robot pose pased on April Tag inputs. Undefined when in Reflective Tape mode.
+   *
+   * @return the 2D pose (translation and rotation) of the robot based on April Tag sightings.
+   */
   public Pose2d getPose2d() {
     return position;
+  }
+
+  /**
+   * Sets the camera mode and pipeline for the type of visual tracking to be used.
+   *
+   * @param mode Provides the scoring mode
+   */
+  public void setScoringMode(ScoringMode mode) {
+    currentScoringMode = mode;
+
+    // set the pipeline to match the scoring mode.
+    if (currentScoringMode == ScoringMode.AprilTag) {
+      // FIXME: verify pipeline indices
+      pipeline.setDouble(1);
+    } else if (currentScoringMode == ScoringMode.ReflectiveTape) {
+      pipeline.setDouble(0);
+    }
+  }
+
+  public ScoringMode getScoringMode() {
+    return currentScoringMode;
   }
 
   public enum ScoringMode {
