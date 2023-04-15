2ac4ed6ba4093cc870e6620c10ecd08adae7e23c
stonem2 <stonem2@muhs.edu>
2023-04-15 10:07:46 -0500

    Ready To Use Auto Align In StateMachine

---
+++ src/main/java/frc/robot/commands/AutoDriving/AutoAlignToScore.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.AutoDriving;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.RobotContainer.RobotRotationMode;
+import frc.robot.RobotContainer.RobotTranslationMode;
+import frc.robot.state_machine.RobotStateMachine;
+import frc.robot.state_machine.events.ScorePressed;
+import frc.robot.subsystems.LimelightScoring;
+
+public class AutoAlignToScore extends CommandBase {
+  RobotContainer robotContainer;
+  RobotStateMachine robotStateMachine;
+  LimelightScoring limelightScoring;
+
+  public AutoAlignToScore(
+      RobotContainer robotContainer,
+      RobotStateMachine robotStateMachine,
+      LimelightScoring limelightScoring) {
+    this.robotContainer = robotContainer;
+    this.robotStateMachine = robotStateMachine;
+    this.limelightScoring = limelightScoring;
+  }
+
+  @Override
+  public void initialize() {
+    robotContainer.robotTranslationMode = RobotTranslationMode.SCORE_PIECE;
+    robotContainer.robotRotationMode = RobotRotationMode.SCORE_PIECE;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    robotStateMachine.fireEvent(new ScorePressed());
+  }
+
+  @Override
+  public boolean isFinished() {
+    return limelightScoring.isAtInterpDistanceSetpoint();
+  }
+}
