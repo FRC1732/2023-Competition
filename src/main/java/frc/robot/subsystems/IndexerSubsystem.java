c3dc0be1dd44c5247be4ab24b9e057da894b0b66
Theo Kachelski <tkachel101@aol.com>
2023-04-20 12:55:43 -0500

    Revert Averaging

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -46,8 +46,6 @@ public class IndexerSubsystem extends SubsystemBase {
   private GenericEntry motorSpeedEntry;
   private boolean brakeMode;
   private double prevSetpoint = 0;
-  private double[] prevCurrent;
-  private int prevCurrentIndex;
 
   private final IndexerIO io;
   private final IndexerIOInputsAutoLoggedv2 inputs = new IndexerIOInputsAutoLoggedv2();
@@ -68,8 +66,6 @@ public class IndexerSubsystem extends SubsystemBase {
     prevSetpoint = indexerRotationMotor.getEncoder().getPosition();
     indexerRotationMotor.getEncoder().setPosition(Constants.INDEXER_STARTING_POSITION);
     // setupShuffleboard();
-    prevCurrent = new double[3];
-    prevCurrentIndex = 0;
     pidController.setP(Constants.INDEXER_ARM_P_VALUE);
     pidController.setI(Constants.INDEXER_ARM_I_VALUE);
     pidController.setD(Constants.INDEXER_ARM_D_VALUE);
@@ -130,13 +126,7 @@ public class IndexerSubsystem extends SubsystemBase {
   }
 
   public boolean hasPiece() {
-    prevCurrent[prevCurrentIndex] = indexerGrabbingMotor.getOutputCurrent();
-    prevCurrentIndex = (prevCurrentIndex + 1) % 3;
-    double total = 0;
-    for (int i = 0; i < 3; i++) {
-      total += prevCurrent[i];
-    }
-    return total / 3 > Constants.INDEXER_PIECE_DETECTION_CURRENT;
+    return indexerGrabbingMotor.getOutputCurrent() > Constants.INDEXER_PIECE_DETECTION_CURRENT;
   }
 
   @Override
